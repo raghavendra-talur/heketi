da0eba0ea4b8c0e22c954399b40e1d596abb68a2
Michael Adam <obnox@redhat.com>
2017-06-14 08:19:20 +0200

    Select only those clusters for block hosting volume creation that sport the Block flag
    if there are any with the flag, otherwise consider all
    
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- apps/glusterfs/block_volume_entry.go
+++ apps/glusterfs/block_volume_entry.go
@@ -185,7 +185,28 @@ func (v *BlockVolumeEntry) Create(db *bolt.DB,
 		err := db.View(func(tx *bolt.Tx) error {
 			var err error
 			possibleClusters, err = ClusterList(tx)
-			return err
+			if err != nil {
+				return err
+			}
+			//
+			// If there are any clusters marked with the Block
+			// flag, then only consider those. Otherwise consider
+			// all clusters.
+			//
+			var blockClusters []string{}
+			for clusterId := range possibleClusters {
+				c, err := NewClusterEntryFromId(tx, clusterId)
+				if err != nil {
+					return err
+				}
+				if c.Block {
+					blockClusters = append(blockClusters, clusterId)
+				}
+			}
+			if blockClusters != nil {
+				possibleClusters = blockClusters
+			}
+			return nil
 
 		})
 		if err != nil {
