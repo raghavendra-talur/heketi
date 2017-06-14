05c1fb4cb1e439c428ac4730da3bbf9051bf1ae9
Michael Adam <obnox@redhat.com>
2017-06-14 22:33:28 +0200

    try and print [block] for block hosting volumes in heketi volume list
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- client/cli/go/cmds/volume.go
+++ client/cli/go/cmds/volume.go
@@ -357,10 +357,15 @@ var volumeListCommand = &cobra.Command{
 					return err
 				}
 
-				fmt.Fprintf(stdout, "Id:%-35v Cluster:%-35v Name:%v\n",
+				blockstr := ""
+				if volume.Block {
+					blockstr = " [block]"
+				}
+				fmt.Fprintf(stdout, "Id:%-35v Cluster:%-35v Name:%v%v\n",
 					id,
 					volume.Cluster,
-					volume.Name)
+					volume.Name,
+					blockstr)
 			}
 		}
 
