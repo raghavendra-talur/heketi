9caffff0676a458cb278a749cff30bcda4976841
John Mulligan <jmulligan@redhat.com>
2018-08-28 13:44:22 -0400

    xxx: deny placing a block volume on a locked bhv
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/block_volume_entry.go
+++ apps/glusterfs/block_volume_entry.go
@@ -379,6 +379,10 @@ func (v *BlockVolumeEntry) Destroy(db wdb.DB, executor executors.Executor) error
 // the volume is incompatible. It returns false, and an error if the
 // database operation fails.
 func canHostBlockVolume(tx *bolt.Tx, bv *BlockVolumeEntry, vol *VolumeEntry) (bool, error) {
+	if vol.Info.BlockInfo.Locked {
+		logger.Warning("Block hosting volume %v is locked")
+		return false, nil
+	}
 	if vol.Info.BlockInfo.FreeSize < bv.Info.Size {
 		logger.Warning("Free size %v is less than the requested block volume size %v",
 			vol.Info.BlockInfo.FreeSize, bv.Info.Size)
