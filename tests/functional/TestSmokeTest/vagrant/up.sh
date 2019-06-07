48e97468844545c6a833682e020a07af2f12eeca
Raghavendra Talur <rtalur@redhat.com>
2019-06-07 13:55:20 -0400

    my changes to vagrant
    Signed-off-by: Raghavendra Talur <rtalur@redhat.com>

--- tests/functional/TestSmokeTest/vagrant/up.sh
+++ tests/functional/TestSmokeTest/vagrant/up.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 set -e
-vagrant up --no-provision "$@"
+vagrant up --no-provision --no-parallel "$@"
 vagrant provision
+
+virsh list --name --all | grep "heketi-vm" | while read each;
+do
+        echo  domain "$each";
+	virsh snapshot-create-as --domain "$each" --atomic --name "deployed"
+done
+
